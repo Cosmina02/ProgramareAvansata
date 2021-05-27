e9c03bcc77a4f4b233d4947576e70ab3fbf83b1f
Iordan Cosmina <58854421+Cosmina02@users.noreply.github.com>
2021-05-27 17:22:22 +0300

    lab13

---
+++ Laborator13/src/com/DisplayLocales.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.util.Locale;
+
+public class DisplayLocales {
+
+    public void start(){
+        Locale[] locales = Locale.getAvailableLocales();
+        for(Locale locale : locales)
+            System.out.println(locale.toString());
+    }
+}
---
+++ Laborator13/src/com/SetLocale.java
@@ -0,0 +1,9 @@
+package com;
+
+import java.util.Locale;
+
+public class SetLocale {
+    public void start(Locale locale){
+        Locale.setDefault(locale);
+    }
+}
